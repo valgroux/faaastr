81e3342e40ee85aa491d131adbb79b7334e97d27
Valentin Groux <valentin.groux.tic@gmail.com>
2024-06-04 23:02:10 +0200

    Mise en place du header

--- wp-content/themes/faaastr/header.php
+++ wp-content/themes/faaastr/header.php
@@ -6,17 +6,32 @@
     <?php wp_head(); ?>
 </head>
 <body>
-    <header>
-        <nav>
-            <?php
-            wp_nav_menu(array(
-                'theme_location' => 'header-desktop-menu',
-                'menu' => 'header-desktop',
-                // 'menu_id'        => 'header-desktop-menu',
-                // 'container'      => 'div',
-                // 'container_class' => 'header-desktop-menu-container',
-                // 'menu_class'     => 'header-desktop-menu',
-            ));
-            ?>
-        </nav>
+    <header class="header">
+        <div class="container--1700">
+            <div class="header__content">
+
+                <div class="header__content__left">
+                    <?php if(is_front_page()) : ?>
+                        <h1>
+                            <a href="<?= esc_url(home_url( '/' )); ?>">
+                                <img src="<?= get_template_directory_uri() ?>/src/img/logo.webp" alt="FAAASTR, Optimisez, Automatisez, Se démarquer" width="154" height="47" loading="lazy">
+                            </a>
+                        </h1>
+                    <?php else : ?>
+                        <a href="<?= esc_url( home_url( '/' )); ?>">
+                            <img src="<?= get_template_directory_uri() ?>/assets/svg/logo.svg" alt="FAAASTR, Optimisez, Automatisez, Se démarquer" width="154" height="47" loading="lazy">
+                        </a>
+                    <?php endif; ?>
+                </div>
+
+                <nav class="header__content__center" role="navigation">
+                    <?php wp_nav_menu( array( 'theme_location' => 'header-desktop-menu', 'menu' => 'header-desktop', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>', 'current_class' => 'current-menu-item' ) ); ?>
+                </nav>
+
+
+                <div class="header__content__right">
+                </div>
+                
+            </div>
+        </div>
     </header>
