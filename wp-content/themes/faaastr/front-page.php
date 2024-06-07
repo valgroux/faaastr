edbd6aef0d2f024b3483966d4a9bc696bccc23df
Valentin Groux <valentin.groux.tic@gmail.com>
2024-06-07 18:15:31 +0200

    Avancement masthead accueil

--- wp-content/themes/faaastr/front-page.php
+++ wp-content/themes/faaastr/front-page.php
@@ -1,4 +1,10 @@
+<?php /* Template Name: Page d'accueil */ ?>
+
 <?php get_header();?>
+
+
+<!-- SECTION MASTHEAD -->
+<?php get_template_part('src/template-parts/home/home-masthead')?>
 
 
 
---
+++ wp-content/themes/faaastr/src/template-parts/home/home-masthead.php
@@ -0,0 +1,37 @@
+<section class="masthead">
+    <div class="container--1700">
+
+        <div class="masthead__content">
+
+            <div class="masthead__content__left">
+
+                <h2 class="h1"><div>Marre d'être <span class="jaune"><i>lent</i></span> ?</div>On développe les outils qui te font <span class="text-highlight">gagner du temps</span> et <div><span class="text-highlight">de l'argent</span></div></h2>
+
+                <p>Découvre le pouvoir de l'automatisation</p>
+
+                <div class="masthead__content__left__links">
+
+                    <button class="cta">
+                        <div class="cta__icon cta__icon--projet"></div>
+                        <a href="#">Tu as un projet ? Parlons en</a>
+                    </button>
+
+                    <button class="cta-secondaire">
+                        <a href="#">Découvre notre méthode</a>
+                    </button>
+
+                </div>
+
+                <!-- <div class="masthead__content__left__quote">
+                    <div class="masthead__content__left__quote__star"></div>
+                    <p>5.0 <span>(+100) clients</span></p>
+                </div> -->
+
+            </div>
+
+            <div class="masthead__content__right"></div>
+
+        </div>
+        
+    </div>
+</section>
