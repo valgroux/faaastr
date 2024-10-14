db1472f60589b9d60af5f4abc1b473310d261413
Valentin Groux <valentin.groux.tic@gmail.com>
2024-10-14 22:06:20 +0200

    Modification footer.php

--- wp-content/themes/faaastr/footer.php
+++ wp-content/themes/faaastr/footer.php
@@ -12,7 +12,7 @@
         </div>
 
         <div class="footer__info">
-            <p>Faaastr.com<br></p>
+            <p>faaastr.com<br></p>
             <p>06 33 38 20 67<br></p>
         </div>
 
